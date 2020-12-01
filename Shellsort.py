a79985078854abc543e585f21d11a1107612ec7b
KorolKirill <71098249+Hehehe3plx@users.noreply.github.com>
2020-12-01 21:22:18 +0200

    Revert "add Shell Sort"
    This reverts commit 1d607d802b0b39ffdfdbeabfe0e336bb504048b1.

--- Shellsort.py
+++ Shellsort.py
@@ -1,23 +1,11 @@
 import time
 
-def shellsort(data):
+
+def shellsort(array):
     start = time.perf_counter()
-    n = len(data)
-    gap = n // 2
-
-    while gap > 0:
-        for i in range(gap, n):
-            temp = data[i]
-            j = i
-            while j >= gap and data[j - gap] > temp:
-                data[j] = data[j - gap]
-                j -= gap
-
-            data[j] = temp
-        gap //= 2
 
 
 
     end = time.perf_counter()
     finalTime = 1000 * (end - start)
-    return "Shellsort for "+ str(len(data)) + " elements took "+ str(round(finalTime,3))+" miliseconds"
+    return "Shellsort for "+ str(len(array)) + " elements took "+ str(round(finalTime,3))+" miliseconds"
