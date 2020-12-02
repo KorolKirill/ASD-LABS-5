18eb2343a9db0ad008f399961ed79951c3f0cf12
KorolKirill <71098249+Hehehe3plx@users.noreply.github.com>
2020-12-02 20:49:04 +0200

    Shellsort
    Shellsort

--- Shellsort.py
+++ Shellsort.py
@@ -3,7 +3,19 @@ import time
 
 def shellsort(array):
     start = time.perf_counter()
+    n = len(data)
+    gap = n // 2
 
+    while gap > 0:
+        for i in range(gap, n):
+            temp = data[i]
+            j = i
+            while j >= gap and data[j - gap] > temp:
+                data[j] = data[j - gap]
+                j -= gap
+
+            data[j] = temp
+        gap //= 2
 
 
     end = time.perf_counter()
